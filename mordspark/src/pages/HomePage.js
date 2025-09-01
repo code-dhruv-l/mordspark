4e3d6cb73c857963d59639905f85bc9bdf77bff8
dhruvlimbasiya.b@gmail.com <limbasiyadhruv735@gmail.com>
2025-09-01 23:20:57 +0530

    fist commit

---
+++ mordspark/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js (or wherever your pages are stored)
+
+import React from 'react';
+import './Homepage.css';
+
+export default function HomePage() {
+  return (
+    <div className="homepage-container">
+      
+      
+    </div>
+  );
+}
